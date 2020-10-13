8ff775fc60f5b29867660d510ce016919bfacc28
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2020-10-13 18:28:06 +0200

    Updated requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
 from setuptools import setup
-from Cython.Build import cythonize
-import numpy
 
 setup(
-    name='rcrawler',
-    version='',
-    packages=['src', 'src.core', 'src.core.util', 'src.core.util.string_utils',
-              'src.core.webhook', 'src.core.webhook.handlers',
-              'src.core.downloader', 'src.core.ast_processing',
-              'src.core.ast_processing.ast_classes', 'src.core.preprocessing',
-              'src.core.preprocessing.preprocessor_class', 'test', 'test.core',
-              'test.core.util'],
-    ext_modules=cythonize(["src/core/ast_processing/ast_classes/*.pyx"],
-                          annotate=True),
-    include_dirs=[numpy.get_include()],
+    name='r_parsing',
+    version='1.1',
+    packages=[''],
     url='',
     license='',
-    author=['David Schwenke', 'Joshua Kraft'],
-    author_email='schwenkedavid@t-online.de',
-    description=''
+    author='joshuakraft',
+    author_email='nathan.joshua.kraft@gmx.de',
+    description='MLoSC 2020'
 )
